860149d1d9e2b5662d276461e769c76e58a29d81
lazydev01 <98304349+lazydev01@users.noreply.github.com>
2022-06-14 08:31:44 +0530

    Create day18extra2.cpp

---
+++ day18/day18extra2.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string s)
+    {
+        string ans = "";
+        char temp = s[0];
+        ans+=temp;
+        for(int i = 1; i<s.size(); i++){
+            if(s[i]!=temp){
+                temp = s[i];
+                ans+=temp;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+  // } Driver Code Ends
